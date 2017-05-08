0b0b7e20858c4ee8d0b70f34d4a8326cb623ec05
Kai Wells <support@kaiwells.me>
2017-05-08 16:32:15 -0700

    all the typos

--- setup_root.sh
+++ setup_root.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set up root
-cp scripts/bash_profile.sh $HOME/.bash_profile;
-cp scripts/root_color.sh   $HOME/.bash_color;
-cp scripts/vimrc.sh        $HOME/.vimrc;
-cp scripts/tmux.conf       $HOME/.tmux.conf;
+cp dotfiles/bash_profile.sh $HOME/.bash_profile;
+cp dotfiles/root_color.sh   $HOME/.bash_color;
+cp dotfiles/vimrc.vim       $HOME/.vimrc;
+cp dotfiles/tmux.conf       $HOME/.tmux.conf;
 
 echo 'source $HOME/.bash_profile' >> $HOME/.bashrc;
 source $HOME/.bashrc;
--- setup_user.sh
+++ setup_user.sh
@@ -10,10 +10,10 @@ echo 'Setting' $USERNAME 'Password';
 passwd $USERNAME;
 
 # Set up bash, vim, and tmux profiles
-cp scripts/bash_profile.sh /home/$USERNAME/.bash_profile;
-cp scripts/user_color.sh   /home/$USERNAME/.bash_color;
-cp scripts/vimrc.sh        /home/$USERNAME/.vimrc;
-cp scripts/tmux.conf       /home/$USERNAME/.tmux.conf;
+cp dotfiles/bash_profile.sh /home/$USERNAME/.bash_profile;
+cp dotfiles/user_color.sh   /home/$USERNAME/.bash_color;
+cp dotfiles/vimrc.vim       /home/$USERNAME/.vimrc;
+cp dotfiles/tmux.conf       /home/$USERNAME/.tmux.conf;
 
 echo 'source $HOME/.bash_profile' >> /home/$USERNAME/.bashrc;
 chown $USERNAME:$USERNAME /home/$USERNAME/.bashrc;
