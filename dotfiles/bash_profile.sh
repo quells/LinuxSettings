fed0e62190f93d0cb9ca1e940e8a9cad78b09d71
Kai Wells <support@kaiwells.me>
2017-05-21 16:27:27 -0700

    dist-update -> dist-upgrade

--- dotfiles/bash_profile.sh
+++ dotfiles/bash_profile.sh
@@ -1,7 +1,7 @@
 
 alias speedtest="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
 alias myip="curl --silent http://icanhazip.com"
-alias update="sudo apt update && sudo apt upgrade && sudo apt dist-update && sudo apt autoremove"
+alias update="sudo apt update && sudo apt upgrade && sudo apt dist-upgrade && sudo apt autoremove"
 alias path="echo $PATH | tr ':' '\n' | sort"
 
 # via the CHOCK
